9f5fd35a3be8849f4fcd80a769be101dce8634f8
Laura Paul <lauraruth0210@gmail.com>
2024-10-04 10:50:54 -0500

    login and wireframe

--- LoginTest2/firebase_login/App.tsx
+++ LoginTest2/firebase_login/App.tsx
@@ -1,18 +1,17 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './app/screens/Login';
-import List from './app/screens/List';
-import Details from './app/screens/Details';
-import { useEffect, useState } from 'react';
-import { onAuthStateChanged, User } from 'firebase/auth';
-import { FIREBASE_AUTH } from './FirebaseConfig';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "./app/screens/Login";
+import List from "./app/screens/List";
+import Details from "./app/screens/Details";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { FIREBASE_AUTH } from "./FirebaseConfig";
 
 const Stack = createNativeStackNavigator();
 
 const InsideStack = createNativeStackNavigator();
 
-function InsideLayout(){
+function InsideLayout() {
   return (
     <InsideStack.Navigator>
       <InsideStack.Screen name="My todos" component={List} />
@@ -25,20 +24,27 @@ export default function App() {
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    onAuthStateChanged(FIREBASE_AUTH, (user) =>{
-      console.log('user', user);
+    onAuthStateChanged(FIREBASE_AUTH, (user) => {
+      console.log("user", user);
       setUser(user);
     });
   }, []);
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Login'>
+      <Stack.Navigator initialRouteName="Login">
         {user ? (
-          <Stack.Screen name="Inside" component={InsideLayout} options={{headerShown: false}} /> 
+          <Stack.Screen
+            name="Inside"
+            component={InsideLayout}
+            options={{ headerShown: false }}
+          />
         ) : (
-          <Stack.Screen name="Login" component={Login} options={{headerShown: false}} /> 
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
         )}
-        
       </Stack.Navigator>
     </NavigationContainer>
   );
